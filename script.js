103e9436a515bc665ba117465b270943c492dbdc
dikhsandwiranggatibong <121065961+Dikhsantibong1432@users.noreply.github.com>
2023-08-01 22:38:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,23 @@ const dataMahasiswa = [
     { nim: "105421110422", nama: "Ridha Novriyana Bhakti", karyaTulis: "Vaskularisasi Extremitas Superior", lulus: true },
     { nim: "105421121922", nama: "Evi Mulia Rezki", karyaTulis: "Myologi Extremitas Inferior", lulus: true },
     { nim: "105421122222", nama: "Surezky Amelia", karyaTulis: "Plexus Brachialis", lulus: true },
+    { nim: "105421101721", nama: "Sunarti Anatza", karyaTulis: "Sistem Endokrin", lulus: true },
+    { nim: "105421112221", nama: "Nurul Amelia. R", karyaTulis: "AIK Gerakan Shalat", lulus: true },
+    { nim: "105421112021", nama: "Mustika Ratu Rahma Sapri", karyaTulis: "Myologi Extremitas Superior", lulus: true },
+    { nim: "105421105521", nama: "Jihan Nur Rahmah Suharto", karyaTulis: "Myologi Extremitas Inferior", lulus: true },
+    { nim: "105421115421", nama: "Muhammad Putra Jaya Fadli Syam", karyaTulis: "Plexus Brachialis", lulus: true },
+    { nim: "105421109421", nama: "Firdah Sri Anugrah", karyaTulis: "Plexus Lumbosacralis", lulus: true },
+    { nim: "105421108521", nama: "Lany Agrivina Ruslan", karyaTulis: "Vaskularisasi Extremitas Superior", lulus: true },
+    { nim: "105421106221", nama: "Waode Syafa Aina Tamsila", karyaTulis: "Vaskularisasi Extremitas Inferior", lulus: true },
+    { nim: "105421110921", nama: "Risal Anugrah", karyaTulis: "Sistem Respirasi", lulus: true },
+    { nim: "105421115321", nama: "Nur Hikmah Anugrah Tahir", karyaTulis: "Sistem Kardiovascular", lulus: true },
+    { nim: "105421106621", nama: "Mutmainna Fadillah", karyaTulis: "Sistem Urogenitalia", lulus: true },
+    { nim: "105421105221", nama: "La Ode Muh Isya Cansero", karyaTulis: "Sistem Gastroenterohepatologi", lulus: true },
+    { nim: "105421103321", nama: "Alif Yasir", karyaTulis: "Sistem Neurologi", lulus: true },
+    { nim: "105421118522", nama: "Afifah Magfirah Jafar", karyaTulis: "Vaskularisasi Extremitas Inferior", lulus: true },
+    { nim: "105421107322", nama: "Muhammad Hidayat Nurwahid", karyaTulis: "Sistem Respirasi", lulus: true },
+    { nim: "105421103122", nama: "Khairun Nafiah Ahliddin", karyaTulis: "Sistem Kardiovascular", lulus: true },
+    { nim: "105421107022", nama: "Fakhrana Aris", karyaTulis: "Sistem Urogenitalia", lulus: true },
 ];
 
 const formNIM = document.getElementById("formNIM");
